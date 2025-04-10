fb453c441ff5538b2f3cff9b245863539c8ce456
Alex Jiasheng Ni <niallni1980@gmail.com>
2025-04-09 22:09:48 -0400

    add grok response

---
+++ experiment_1/preprocess_dataset.py
@@ -0,0 +1,29 @@
+import argparse
+import json
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file_path", type=str, default="reference_chinese_corpus.json")
+    parser.add_argument("--output_file_path", type=str, default="model_input_chinese_corpus.json")
+    args = parser.parse_args()
+
+
+
+
+    # 读取json文件
+    with open(args.input_file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    
+
+    processed_data = [
+        {
+            "Question": record["Question"],
+            "question_id": record["question_id"],
+        }
+        for record in data
+    ]
+
+    with open(args.output_file_path, "w", encoding="utf-8") as f:
+        json.dump(processed_data, f, ensure_ascii=False, indent=4)
+
